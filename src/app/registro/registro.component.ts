a16d926c6de4d3815c0b43e5e23e2f9977c7c733
robinson.delonmrl@uanl.edu.mx <robindd2>
2023-11-13 20:09:26 -0600

    ROBIN

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.scss'],
 })
 export class RegistroComponent  implements OnInit {
-
+  
+  dependencias = ['Facultad de Arquitectura',
+  'Facultad de Artes Visuales',
+  'Facultad de Ciencias Biologicas',
+  'Facultad de Ciencias de la Comunicacion',
+  'Facultad de Ciencias Fisico Matematicas',
+  'Facultad de Ciencias Quimicas',
+  'Facultad de Contaduria Publica y Administracion',
+  'Facultad de Derecho y Criminologia',
+  'Facultad de Economia',
+  'Facultad de Enfermeria',
+  'Facultad de Filosofia y Letras',
+  'Facultad de Ingenieria Civil',
+  'Facultad de Ingenieria Mecanica y Electrica',
+  'Facultad de Medicina',
+  'Facultad de Musica',
+  'Facultad de Odontologia',
+  'Facultad de Psicologia',
+  'Facultad de Salud Publica y Nutricion',
+  'Rectoria'];
+  
   constructor() { }
 
   ngOnInit() {}
