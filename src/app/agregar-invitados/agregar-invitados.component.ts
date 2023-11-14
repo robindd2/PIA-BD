f5977e97ae30fde5ad2d9d7453df619be89061e9
robinson.delonmrl@uanl.edu.mx <robindd2>
2023-11-14 16:02:27 -0600

    ROBIN

--- src/app/agregar-invitados/agregar-invitados.component.ts
+++ src/app/agregar-invitados/agregar-invitados.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-invitados.component.scss'],
 })
 export class AgregarInvitadosComponent  implements OnInit {
+  dependencias = ['Facultad de Arquitectura',
+  'Facultad de Artes Visuales',
+  'Facultad de Ciencias Biologicas',
+  'Facultad de Ciencias de la Comunicacion',
+  'Facultad de Ciencias Fisico Matematicas',
+  'Facultad de Ciencias Quimicas',
+  'Facultad de Contaduria Publica y Administracion',
+  'Facultad de Derecho y Criminologia',
+  'Facultad de Economia',
+  'Facultad de Enfermeria',
+  'Facultad de Filosofia y Letras',
+  'Facultad de Ingenieria Civil',
+  'Facultad de Ingenieria Mecanica y Electrica',
+  'Facultad de Medicina',
+  'Facultad de Musica',
+  'Facultad de Odontologia',
+  'Facultad de Psicologia',
+  'Facultad de Salud Publica y Nutricion',
+  'Rectoria'];
 
+  ocupaciones = ['Alumno',
+  'Maestro',
+  'Director',
+  'SubDirector',
+  'Secretaria',
+  'Jefes de Academia',
+  'Administrativo'];
   constructor() { }
 
   ngOnInit() {}
